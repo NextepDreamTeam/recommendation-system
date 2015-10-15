896dcefc4dfee977d07aee8c8a138231cda16056
andeliero92 <andeliero92@gmail.com>
2015-10-15 09:48:08 +0200

    template test

--- app/recommendationsystem/models/storage/TagsDao.scala
+++ app/recommendationsystem/models/storage/TagsDao.scala
@@ -2,17 +2,17 @@ package recommendationsystem.models.storage
 
 import com.orientechnologies.orient.core.command.OCommandRequest
 import com.orientechnologies.orient.core.sql.OCommandSQL
-import com.tinkerpop.blueprints.impls.orient.OrientDynaElementIterable
+import com.tinkerpop.blueprints.impls.orient.{OrientVertex, OrientDynaElementIterable}
 
 //import recommendationsystem.models.Tag
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
-
+import scala.collection.JavaConverters._
 /**
  * Created by aandelie on 14/10/15.
  */
 trait TagsDao {
-  def count: Future[Int]
+  def count: Future[Long]
 
   //def save(e: Tag, upsert: Boolean = false): Future[Boolean]
 
@@ -27,11 +27,14 @@ trait TagsDao {
 
 
 object TagsOdb extends TagsDao{
-  override def count: Future[Int] = {
+  override def count: Future[Long] = {
     val graph = Odb.factory.getNoTx
-    val c: OrientDynaElementIterable = graph.command(new OCommandSQL("select from Tags")).execute()
-    println(c.iterator().next().toString)
-    Future {42}
+    val c: OrientDynaElementIterable = graph.command(new OCommandSQL("select count(*) from Tags")).execute()
+    val it = c.iterator().asScala
+    var count = 0L
+    for (e <- it) count = e.asInstanceOf[OrientVertex].getProperty("count")
+    graph.shutdown()
+    Future {count}
   }
 
   //override def update(oldTag: Tag, newTag: Tag): Future[Boolean] = ???
--- test/recommendationsystem/models/storage/TagsOdb$Test.scala
+++ test/recommendationsystem/models/storage/TagsOdb$Test.scala
@@ -1,12 +1,16 @@
 package recommendationsystem.models.storage
 
+import scala.util.{Success, Failure}
+import scala.concurrent.ExecutionContext.Implicits.global
 /**
  * Created by aandelie on 14/10/15.
  */
 class TagsOdb$Test extends org.scalatest.FunSuite {
   test("TagsOdb.count is invoked") {
-    val i = TagsOdb.count
-    println(i)
-    assert(true)
+    val fres = TagsOdb.count
+    fres onComplete {
+      case Success(count) => assert(count == 1L)
+      case Failure(t) => println("An error has occured: " + t.getMessage)
+    }
   }
 }
