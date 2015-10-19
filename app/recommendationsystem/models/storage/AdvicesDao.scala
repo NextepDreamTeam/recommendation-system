9e6108ad47e116d1170fdc560539b7d5b11ed82c
andeliero92 <andeliero92@gmail.com>
2015-10-19 18:04:36 +0200

    fine AdviceDao

--- app/recommendationsystem/models/storage/AdvicesDao.scala
+++ app/recommendationsystem/models/storage/AdvicesDao.scala
@@ -17,13 +17,13 @@ trait AdvicesDao {
 
   def save(e: Advice, upsert: Boolean = false): Future[Boolean]
 
-  //def update(newTag: Advice, oldTag: Advice): Future[Boolean]
+  def update(newAdvice: Advice): Future[Boolean]
 
-  //def remove(e: Advice): Future[Boolean]
+  def remove(e: Advice): Future[Boolean]
 
-  //def all: Future[List[Advice]]
+  def all: Future[List[Advice]]
 
-  //def find(id: String): Future[List[Advice]]
+  def find(id: String): Future[Option[Advice]]
 }
 
 object AdvicesOdb extends AdvicesDao {
@@ -34,45 +34,130 @@ object AdvicesOdb extends AdvicesDao {
     Future{count}
   }
 
-  //override def update(newTag: Advice, oldTag: Advice): Future[Boolean] = ???
-
-  /*override def all: Future[List[Advice]] = {
+  override def update(newAdvice: Advice): Future[Boolean] = {
     val graph = Odb.factory.getTx
+    val adviceVertex = graph.getVertices("Advices.aid",newAdvice.id).asScala.head
+    adviceVertex.setProperty("date",newAdvice.date)
+    adviceVertex.setProperty("type",newAdvice.kind)
+    //update user
+    val adviceUserEdge = adviceVertex.getEdges(Direction.OUT,"AdviceUser").asScala.head
+    val userVertex = adviceUserEdge.getVertex(Direction.IN)
+    val actualUser = userVertex.getProperty("uid").toString
+    if(! actualUser.equals(newAdvice.user.id)) {
+      val newUserVertex = graph.getVertices("Users.uid",newAdvice.user.id).asScala.head
+      adviceUserEdge.remove()
+      graph.addEdge(null,adviceVertex,newUserVertex,"AdviceUser")
+    }
+    //update tags
+    val tagsAdviceEdges = adviceVertex.getEdges(Direction.OUT,"AdviceOutput").asScala
+    val tagsAdviceVertex = tagsAdviceEdges.map(v => v.getVertex(Direction.IN))
+    val outputEdgesVertices = tagsAdviceEdges zip tagsAdviceVertex
+    val tagList = newAdvice.output.map(x => x._1)
+
+    println("Tag presenti nel db")
+    tagsAdviceVertex.map(x => println(x.getProperty("tag").toString))
+
+    //tags to add into db
+    val tagsToAdd = tagList.filter(
+      t => ! tagsAdviceVertex.map(x => x.getProperty("tag").toString).contains(t.flatten)
+    )
+    //adding missing edges
+    println("Tag aggiunti:")
+    for (t <- tagsToAdd ) {
+      println(t.flatten)
+      //println(adviceVertex.getProperty("aid"))
+      val tagVertex = graph.getVertices("Tags.tag",t.flatten).asScala.head
+      graph.addEdge(null,adviceVertex,tagVertex,"AdviceOutput")
+    }
+
+    //edges to remove from db
+    val edgesToRemove = outputEdgesVertices.filter( oev => !tagList.map(t => t.flatten).contains(oev._2.getProperty("tag")) ).map(ev => ev._1)
+    //removing edges
+    println("tag rimossi:")
+    edgesToRemove.map(x => x.getVertex(Direction.IN)).map(v => println(v.getProperty("tag")))
+    edgesToRemove.map(e => e.remove())
+    graph.commit()
+    graph.shutdown()
+    Future{true}
+  }
+
+  override def all: Future[List[Advice]] = {
+    val graph = Odb.factory.getNoTx
     ODatabaseRecordThreadLocal.INSTANCE.set(graph.getRawGraph)
-    val vlst: Iterable[Vertex] = graph.getVerticesOfClass("Advice").asScala
-    graph commit;
-    //val lst = vlst map (v => Advice()) toList ;
-    val lst = vlst map (av => {
-      //get tags of thi advice
-      val tagsAdviceVertex = av.getEdges(Direction.OUT,"AdviceOutput").asScala.map(v => v.getVertex(Direction.OUT))
+    val vlst: Iterable[Vertex] = graph.getVerticesOfClass("Advices").asScala
+    val lst = vlst map (adviceVertex => {
+      //get tags of this advice
+      val tagsAdviceVertex = adviceVertex.getEdges(Direction.OUT,"AdviceOutput").asScala
+        .map(v => v.getVertex(Direction.OUT))
       val output = tagsAdviceVertex map (x => (x.getProperty("tag"),0D)) toList
 
       //get user information
-      val uv = av.getEdges(Direction.OUT,"AdviceUser").asScala.map(v => v.getVertex(Direction.OUT)).head
+      val userVertex = adviceVertex.getEdges(Direction.OUT,"AdviceUser").asScala.map(v => v.getVertex(Direction.OUT)).head
       //user must be one
-      val userTagsEdge = av.getEdges(Direction.OUT,"HoldsTag").asScala
+      val userTagsEdge = adviceVertex.getEdges(Direction.OUT,"HoldsTag").asScala
       val userTagsVertex = userTagsEdge map (e => e.getVertex(Direction.OUT))
       val tagList = userTagsEdge zip userTagsVertex map
         (x => (x._2.getProperty("tag"),x._1.getProperty("weight"),x._1.getProperty("lastInsert"))) toList
-      val user = User(uv.getProperty("uid"),uv.getProperty("email"),None,Option(tagList))
+      val user = User(userVertex.getProperty("uid"),userVertex.getProperty("email"),None,Option(tagList))
 
-      Advice(av.getProperty("aid"),user,output,av.getProperty("date"),av.getProperty("type"))
+      Advice(
+        adviceVertex.getProperty("aid"),
+        user,output,
+        adviceVertex.getProperty("date"),
+        adviceVertex.getProperty("type")
+      )
     })
     graph.shutdown
     Future{lst.toList}
-  }*/
+  }
 
-  //override def remove(e: Advice): Future[Boolean] = ???
+  override def remove(e: Advice): Future[Boolean] = {
+    val graph = Odb.factory.getTx
+    val adviceVertex = graph.getVertices("Advices.aid",e.id).asScala.head
+    graph.removeVertex(adviceVertex)
+    graph.commit()
+    Future{true}
+  }
 
   override def save(e: Advice, upsert: Boolean): Future[Boolean] = {
     val graph = Odb.factory.getTx
     val adviceVertex = graph.addVertex("Advices",null)
     adviceVertex.setProperty("aid",e.id)
     adviceVertex.setProperty("date",e.date)
-    adviceVertex.setProperty("system",e.kind)
+    adviceVertex.setProperty("type",e.kind)
+    //adding AdviceUser edge
+    val userVertex = graph.getVertices("Users.uid",e.user.id).asScala.head //must be one
+    val adviceUserEdge = graph.addEdge(null,adviceVertex,userVertex,"AdviceUser")
+    //adding AdviceOutput edge
+    for ( (tag, pt) <- e.output ) {
+      val tagVertex = graph.getVertices("Tags.tag",tag.flatten).asScala.head
+      val adviceOutputEdge = graph.addEdge(null,adviceVertex,tagVertex,"AdviceOutput")
+    }
     graph.commit
+    graph.shutdown
     Future{true}
   }
 
-  //override def find(id: String): Future[List[Advice]] = ???
+  override def find(id: String): Future[Option[Advice]] = {
+    val graph = Odb.factory.getNoTx
+    val adviceVertices = graph.getVertices("Advices.aid",id).asScala
+    if(adviceVertices.isEmpty)
+      Future{None}
+    else {
+      val adviceVertex = adviceVertices.head //must be one
+      val userVertex = adviceVertex.getEdges(Direction.OUT,"AdviceUser").asScala
+          .map(v => v.getVertex(Direction.OUT)).head
+      val tagsAdviceVertex = adviceVertex.getEdges(Direction.OUT,"AdviceOutput").asScala
+        .map(v => v.getVertex(Direction.OUT))
+      val output = tagsAdviceVertex map (x => (x.getProperty("tag"),0D)) toList
+      val advice = Advice(
+        adviceVertex.getProperty("aid"),
+        userVertex.getProperty("uid"),
+        output,
+        adviceVertex.getProperty("date"),
+        adviceVertex.getProperty("type")
+      )
+      Future{Option(advice)}
+    }
+  }
 }
--- test/recommendationsystem/models/storage/AdvicesOdb$Test.scala
+++ test/recommendationsystem/models/storage/AdvicesOdb$Test.scala
@@ -13,47 +13,95 @@ import scala.util.{Failure, Success}
  */
 class AdvicesOdb$Test extends FunSuite with BeforeAndAfterEach {
 
-  /*val advice = {
-    val user = User("utendeTest",Option("utente@test"),None,Option((Tag("tag","caso"),0D,0L)::List()))
-    Advice(
-      "idDaEliminare",
-      user,
-      (Tag("tag","caso"),0D)::(Tag("caso","tag"),0D)::List(),
-      Calendar.getInstance().getTimeInMillis,true,"system"
-    )
-  }*/
+  val t1 = Tag("tag", "caso")
+  val t2 = Tag("caso", "tag")
+  val user1 = User("utenteTest", Option("utente@test"), None, Option((t1, 0D, 0L) :: List()))
+  val user2 = User("utendeTest", Option("utende@test"), None, Option((t1, 0D, 0L) :: List()))
+  val advice1 = Advice(
+    "idDaEliminare",
+    user1,
+    (t2, 0D) :: List(),
+    Calendar.getInstance().getTimeInMillis, true, "system"
+  )
+  val advice2 = Advice(
+    "idDaEliminaree",
+    user2,
+    (t1, 0D) ::(t2, 0D) :: List(),
+    Calendar.getInstance().getTimeInMillis, true, "system"
+  )
 
-  /*override def beforeEach() {
-
+  override def beforeEach() {
+    TagsOdb.save(t1)
+    TagsOdb.save(t2)
+    UsersOdb.save(user1)
+    UsersOdb.save(user2)
   }
 
   override def afterEach() {
+    UsersOdb.remove(user1)
+    UsersOdb.remove(user2)
+    TagsOdb.remove(t1)
+    TagsOdb.remove(t2)
+  }
 
-  }*/
-
-  /*test("AdviceOdb.count is invoked") {
+  test("AdviceOdb.count is invoked") {
+    AdvicesOdb.save(advice1)
+    AdvicesOdb.save(advice2)
     val fres = AdvicesOdb.count
     fres onComplete {
-      case Success(x) => assert(x==0)
-      case Failure(t) => println("An error has occured: " + t.getMessage)
+      case Success(x) => assert(x==2)
+      case Failure(t) => assert(false); println("An error has occured: " + t.getMessage)
     }
-  }*/
+    AdvicesOdb.remove(advice1)
+    AdvicesOdb.remove(advice2)
+  }
 
-  /*test("AdviceOdb.all is invoked") {
+  test("AdviceOdb.all is invoked") {
+    AdvicesOdb.save(advice1)
+    AdvicesOdb.save(advice2)
     val fres = AdvicesOdb.all
     fres onComplete {
-      //TODO improve
-      case Success(x) => assert(x.size==0)
+      case Success(x) => assert(x.size==2)
       case Failure(t) => println("An error has occured: " + t.getMessage)
     }
-  }*/
+    AdvicesOdb.remove(advice1)
+    AdvicesOdb.remove(advice2)
+  }
 
-  /*test("AdviceOdb.save is invoked") {
-    val fres = AdvicesOdb.save(advice)
-    fres onComplete {
+  test("AdviceOdb.save and remove is invoked") {
+    val fResSave = AdvicesOdb.save(advice1)
+    fResSave onComplete {
       case Success(x) => assert(x)
       case Failure(t) => println("An error has occured: " + t.getMessage)
     }
-  }*/
+    val fResFind = AdvicesOdb.find(advice1.id)
+    fResFind onComplete {
+      case Success(o) => o match {
+        case Some(x) => assert (true)
+        case None => assert(false)
+      }
+    }
+    val fResRemove = AdvicesOdb.remove(advice1)
+    fResRemove onComplete {
+      case Success(x) => assert(x)
+      case Failure(t) => println("An error has occured: " + t.getMessage); assert(false)
+    }
+  }
+
+  test("AdviceOdb.update is invoked") {
+    AdvicesOdb.save(advice1)
+    val advice3 = Advice(
+      "idDaEliminare",
+      user2,
+      (t1, 0D) ::(t2, 0D) :: List(),
+      Calendar.getInstance().getTimeInMillis, true, "system"
+    )
+    val fResUpdate = AdvicesOdb.update(advice3)
+    fResUpdate onComplete {
+      case Success(x) => assert(x)
+      case Failure(t) => println("An error has occured: " + t.getMessage); assert(false)
+    }
+    AdvicesOdb.remove(advice3)
+  }
 
 }
