8329cd3a6d5a52c77e535446d7f3011a6aff5ab6
andeliero92 <andeliero92@gmail.com>
2015-10-23 12:01:15 +0200

    clearDb method added

--- app/recommendationsystem/models/storage/Odb.scala
+++ app/recommendationsystem/models/storage/Odb.scala
@@ -1,10 +1,16 @@
 package recommendationsystem.models.storage
 
 import com.tinkerpop.blueprints.impls.orient.OrientGraphFactory
+import scala.collection.JavaConverters._
 
 /**
  * Created by aandelie on 14/10/15.
  */
 object Odb {
   val factory = new OrientGraphFactory("remote:localhost:2424/recommendation-system","root","root").setupPool(1,10)
+
+  def clearDb = {
+    val graph = factory.getNoTx
+    val vertices = graph.getVertices().asScala.map(v => v.remove())
+  }
 }
